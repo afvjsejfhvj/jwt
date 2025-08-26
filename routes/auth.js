5fb0f0d2c9db2458a70d03fc24e597fcfa75d29a
mateus <150347661+afvjsejfhvj@users.noreply.github.com>
2025-08-26 08:52:47 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,11 @@
 //rotas de autenticação
+//rotas de autenticação
+
+import express from 'express';
+
+const routes = express.Router();
+
+routes.post('/register', register);
+routes.post('/login', login);
+
+export default routes;
